6f768774a5e8a1a9b22786beac0c73a174b1c882
alirezaseif28 <alirezaseif28@gmail.com>
2023-01-05 10:09:42 +0330

    add package Entityframeworkcore

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Post
+    {
+        public long ID { get; set; }
+        public string? Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
