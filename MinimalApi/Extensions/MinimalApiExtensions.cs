5b5251ae82d1b63a72e9da1f290c27eb1abbc411
alirezaseif28 <alirezaseif28@gmail.com>
2023-01-06 16:31:27 +0330

    Add Extensions

---
+++ MinimalApi/Extensions/MinimalApiExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Posts.Commands;
+
+namespace MinimalApi.Extensions
+{
+    public static class MinimalApiExtensions
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder) {
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+
+            builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(connectionString));
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddMediatR(typeof(CreatePost));
+        }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,19 +1,11 @@
-using Application.Abstractions;
-using DataAccess;
-using DataAccess.Repositories;
-using Microsoft.EntityFrameworkCore;
 using MediatR;
 using Application.Posts.Commands;
 using Application.Posts.Queries;
 using Domain.Models;
+using MinimalApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("Default");
-
-builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(connectionString));
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-builder.Services.AddMediatR(typeof(CreatePost));
+builder.RegisterServices();
 
 // Add services to the container.
 
