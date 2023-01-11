e79170f46aedc0c49b73234a77f2a62889e597b8
alirezaseif28 <alirezaseif28@gmail.com>
2023-01-11 19:26:55 +0330

    add middleware error handeling

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -7,6 +7,19 @@ builder.RegisterServices();
 
 var app = builder.Build();
 
+app.Use(async (ctx,next) => {
+	try
+	{
+		await next();
+	}
+	catch (Exception)
+	{
+		ctx.Response.StatusCode = 500;
+		await ctx.Response.WriteAsync("An error ocurred");
+	}
+});
+
+
 app.RegisterEndpointDefinitions();
 
 app.Run();
